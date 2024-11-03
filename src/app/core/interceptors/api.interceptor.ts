735b7c8743341a5177eb6e0195df9ed8777bf5a3
MichaelSoquat <michael.soquat@gmx.de>
2024-11-03 14:57:55 +0100

    updated api url

--- src/app/core/interceptors/api.interceptor.ts
+++ src/app/core/interceptors/api.interceptor.ts
@@ -13,7 +13,7 @@ export class ApiInterceptor implements HttpInterceptor {
     req: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    const apiReq = req.clone({ url: `http://localhost:8000/${req.url}` });
+    const apiReq = req.clone({ url: `http://localhost:8000${req.url}` });
     return next.handle(apiReq);
   }
 }
