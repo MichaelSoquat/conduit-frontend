103292952886821cedd884fffd50d5c9861bd5ef
MichaelSoquat <michael.soquat@gmx.de>
2024-11-03 15:58:31 +0100

    update

--- src/app/core/interceptors/api.interceptor.ts
+++ src/app/core/interceptors/api.interceptor.ts
@@ -6,7 +6,7 @@ import {
   HttpRequest,
 } from "@angular/common/http";
 import { Observable } from "rxjs";
-import { environment } from "src/environments/environment.dev";
+import { environment } from "src/environments/environment";
 
 @Injectable({ providedIn: "root" })
 export class ApiInterceptor implements HttpInterceptor {
